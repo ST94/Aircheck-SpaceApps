15a982780d8d8c8e1003d85777f9773ccbef6948
ST94 <ST94.dev@gmail.com>
2016-04-23 11:34:46 -0400

    Done simple mockup of local air quality levels. Waiting on API

--- AirWatch/app/src/main/java/com/gmail/st94/dev/airwatch/MainActivity.java
+++ AirWatch/app/src/main/java/com/gmail/st94/dev/airwatch/MainActivity.java
@@ -1,13 +1,127 @@
 package com.gmail.st94.dev.airwatch;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    String mCountryOutput;
+    String mCity;
+    Button mReportSymptomsButton;
+
+    LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        // Get the location passed to this service through an extra.
+
+        Location location = getLastKnownLocation();
+
+        List<Address> addresses = null;
+        String errorMessage = "";
+
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            Address address = addresses.get(0);
+            Log.i(TAG, getString(R.string.address_found));
+            mCountryOutput = address.getCountryName();
+            mCity = address.getLocality();
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems.
+            errorMessage = getString(R.string.no_address_found);
+            Log.e(TAG, errorMessage, ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch invalid latitude or longitude values.
+            Log.e(TAG, errorMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
+        }
+        Log.i(TAG, "Found country code: " + mCountryOutput);
+
+        mReportSymptomsButton = (Button) findViewById(R.id.report_symptoms_button);
+        mReportSymptomsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (getApplicationContext(), FormActivity.class);
+                startActivity (i);
+
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+    }
+
+    private Location getLastKnownLocation() {
+        mLocationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            Location l = null;
+            try {
+                 l = mLocationManager.getLastKnownLocation(provider);
+            }
+            catch (SecurityException scex) {
+                Log.e (TAG, scex.toString());
+            }
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+                Log.i(TAG, "Using provider: " + provider);
+            }
+        }
+        return bestLocation;
+    }
+
 }
